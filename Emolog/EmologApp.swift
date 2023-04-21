dd2bfd3fc10f05eb33de61935c6891227b251a28
44masa <kotodama47@gmail.com>
2023-04-21 15:07:16 +0900

    Initial Commit

---
+++ Emolog/EmologApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmologApp.swift
+//  Emolog
+//
+//  Created by 荒武禎将 on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct EmologApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
